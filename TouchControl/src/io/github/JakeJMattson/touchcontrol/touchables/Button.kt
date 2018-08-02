830cd3bc3789898f9ef9aac1b22e667d1ff66cd3
Jake <JakeJMattson@gmail.com>
2018-08-01 22:56:59 -0500

    Converted to Kotlin
    Converted abstract subclasses from Java to Kotlin.

---
+++ TouchControl/src/io/github/JakeJMattson/touchcontrol/touchables/Button.kt
@@ -0,0 +1,33 @@
+package io.github.JakeJMattson.touchcontrol.touchables
+
+import org.opencv.core.*
+
+/**
+ * Abstract Button - contains general (clickable) button behavior.
+ *
+ * @author JakeJMattson
+ */
+abstract class Button protected constructor(dimensions: Rect, color: Scalar) : Touchable(dimensions, color) {
+	/**
+	 * Whether or not a non-background pixel is currently being detected
+	 */
+	protected var isBeingClicked: Boolean = false
+
+	/*
+	 * (non-Javadoc)
+	 * @see io.github.JakeJMattson.touchcontrol.touchables.Touchable
+	 * #updateDetectionPoint(org.opencv.core.Mat)
+	 */
+	override fun updateDetectionPoint(filteredImage: Mat): Point? {
+		//Update state
+		super.updateDetectionPoint(filteredImage)
+
+		//Determine if button is currently detecting non-background pixel
+		if (hasDetection() && !isBeingClicked)
+			isBeingClicked = true
+		else if (!hasDetection() && isBeingClicked)
+			isBeingClicked = false
+
+		return detectionPoint
+	}
+}
---
+++ TouchControl/src/io/github/JakeJMattson/touchcontrol/touchables/ToggleButton.kt
@@ -0,0 +1,58 @@
+package io.github.JakeJMattson.touchcontrol.touchables
+
+import org.opencv.core.*
+
+/**
+ * Abstract Button - contains general (toggle) button behavior.
+ *
+ * @author JakeJMattson
+ */
+abstract class ToggleButton protected constructor(dimensions: Rect, color: Scalar) : Button(dimensions, color) {
+	/**
+	 * The current state of the button
+	 */
+	protected var isToggledOn: Boolean = false
+	/**
+	 * Whether or not the button has already switched states internally
+	 */
+	private var hasSwitched: Boolean = false
+
+	init {
+		isToggledOn = false
+		hasSwitched = false
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * io.github.JakeJMattson.touchcontrol.touchables.Button
+	 * #updateDetectionPoint(org.opencv.core.Mat)
+	 */
+	override fun updateDetectionPoint(filteredImage: Mat): Point? {
+		//Save previous state
+		val wasClicked = isBeingClicked
+
+		//Update state
+		super.updateDetectionPoint(filteredImage)
+
+		//Toggle state
+		if (isBeingClicked && !wasClicked) {
+			if (!hasSwitched) {
+				isToggledOn = !isToggledOn
+				hasSwitched = true
+			}
+		}
+		else
+			hasSwitched = false
+
+		return detectionPoint
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see io.github.JakeJMattson.touchcontrol.touchables.Button#toString()
+	 */
+	override fun toString(): String {
+		return super.toString() + format("Toggled on:", isToggledOn)
+	}
+}
