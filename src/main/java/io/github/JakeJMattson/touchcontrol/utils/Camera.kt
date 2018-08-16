95d7e8905dd992b01b4d313a56cb4512910b40e9
Jake <JakeJMattson@gmail.com>
2018-08-16 01:28:31 -0500

    Convert to Kotlin
    Converted Camera wrapper to Kotlin

---
+++ src/main/java/io/github/JakeJMattson/touchcontrol/utils/Camera.kt
@@ -0,0 +1,59 @@
+package io.github.JakeJMattson.touchcontrol.utils
+
+import org.opencv.core.Core
+import org.opencv.core.Mat
+import org.opencv.videoio.VideoCapture
+import org.opencv.videoio.Videoio
+
+import javax.swing.*
+
+/**
+ * Utility class containing functionality related to OpenCV VideoCapture.
+ *
+ * @author JakeJMattson
+ */
+class Camera @JvmOverloads constructor(
+
+	private val rotation: Int? = null, cameraIndex: Int = 0) {
+
+	/**
+	 * OpenCV VideoCapture to access physical camera
+	 */
+	private val videoCapture: VideoCapture = VideoCapture(cameraIndex)
+
+	val width: Double
+		get() = videoCapture.get(Videoio.CAP_PROP_FRAME_WIDTH)
+
+	val height: Double
+		get() = videoCapture.get(Videoio.CAP_PROP_FRAME_HEIGHT)
+
+	val isOpened: Boolean
+		get() = videoCapture.isOpened
+
+	/**
+	 * Retrieve a frame (OpenCV matrix) from the camera.
+	 *
+	 * @return Frame
+	 */
+	val frame: Mat
+		get() {
+			val frame = Mat()
+			videoCapture.read(frame)
+
+			if (rotation != null)
+				Core.flip(frame, frame, rotation)
+
+			return frame
+		}
+
+	init {
+		if (!isOpened)
+			JOptionPane.showMessageDialog(null, "No camera detected!", "Missing hardware",
+				JOptionPane.ERROR_MESSAGE)
+	}
+
+	/**
+	 * Return camera control to the operating system.
+	 */
+	fun release() = videoCapture.release()
+}
