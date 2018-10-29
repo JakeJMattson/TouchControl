b729eefeefa0ab07686daadcde9d1d81ce231e77
Jake <JakeJMattson@gmail.com>
2018-10-28 22:41:31 -0500

    Clean demo package

--- src/main/java/io/github/jakejmattson/touchcontrol/demo/Demos.java
+++ src/main/java/io/github/jakejmattson/touchcontrol/demo/Demos.java
@@ -13,44 +13,31 @@ final class Demos
 
 	static TouchableGroup createMouseDemo(double cameraWidth, double cameraHeight, Scalar color)
 	{
-		//Create component dimensions
 		Point topLeft = new Point(PADDING, PADDING);
 		Point bottomRight = new Point(cameraWidth - PADDING, cameraHeight - PADDING);
 		Rect mousePadRect = new Rect(topLeft, bottomRight);
 
-		//Create component
 		MousePad mousePad = new MousePad(mousePadRect, color);
-
-		//Create group
 		return new TouchableGroup(mousePad);
 	}
 
 	static TouchableGroup createPianoDemo(double cameraWidth, double cameraHeight, Scalar color)
 	{
-		//Create group
 		TouchableGroup group = new TouchableGroup();
-
-		//Keys for demo piano
 		char[] notes = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
 
-		//Create keys
 		for (int i = 0; i < notes.length; i++)
 		{
-			//Base positions
 			double x1 = (double) i / notes.length * cameraWidth;
 			double x2 = (double) (i + 1) / notes.length * cameraWidth;
 			double y1 = 1 / 4.0 * cameraHeight;
 			double y2 = 3 / 4.0 * cameraHeight;
 
-			//Create component dimensions
 			Point topLeft = new Point(x1 + PADDING, y1 + PADDING);
 			Point bottomRight = new Point(x2 - PADDING, y2 - PADDING);
 			Rect keyRect = new Rect(topLeft, bottomRight);
 
-			//Create component
 			PianoKey key = new PianoKey(keyRect, color, notes[i]);
-
-			//Add component to group
 			group.addComponent(key);
 		}
 
@@ -59,15 +46,11 @@ final class Demos
 
 	static TouchableGroup createVolumeDemo(double cameraWidth, double cameraHeight, Scalar color)
 	{
-		//Create component dimensions
 		Point topLeft = new Point(0.4 * cameraWidth, PADDING);
 		Point bottomRight = new Point(0.6 * cameraWidth - PADDING, cameraHeight - PADDING);
 		Rect volumeSliderRect = new Rect(topLeft, bottomRight);
 
-		//Create component
 		VolumeSlider volumeSlider = new VolumeSlider(volumeSliderRect, color);
-
-		//Create group
 		return new TouchableGroup(volumeSlider);
 	}
 }
