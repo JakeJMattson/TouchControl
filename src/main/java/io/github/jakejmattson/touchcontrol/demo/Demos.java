5b6c278a4d57533959fa77d41d21ba1f707f94f1
Jacob Mattson <JakeJMattson@gmail.com>
2018-11-01 12:37:54 -0500

    Redesign Grid
    Redesigned Grid class to calculate dimensions instead of manipulating Touchable objects directly.

--- src/main/java/io/github/jakejmattson/touchcontrol/demo/Demos.java
+++ src/main/java/io/github/jakejmattson/touchcontrol/demo/Demos.java
@@ -14,8 +14,8 @@ final class Demos
 	static TouchableGroup createMouseDemo(double cameraWidth, double cameraHeight, Scalar color)
 	{
 		Grid grid = new Grid(1, 1, cameraWidth, cameraHeight, PADDING);
-		MousePad mousePad = new MousePad(new Rect(), color);
-		grid.align(mousePad, new Point(0, 0), new Point(0, 0));
+		Rect dimensions = grid.calculateDimensions(new Point(0, 0), new Point(0, 0));
+		MousePad mousePad = new MousePad(dimensions, color);
 
 		return new TouchableGroup(mousePad);
 	}
@@ -28,8 +28,8 @@ final class Demos
 
 		for (int i = 0; i < keyCount; i++)
 		{
-			PianoKey key = new PianoKey(new Rect(), color, (char) (i + 'A'));
-			grid.align(key, new Point(i, 1), new Point(i, 3));
+			Rect dimensions = grid.calculateDimensions(new Point(i, 1), new Point(i, 3));
+			PianoKey key = new PianoKey(dimensions, color, (char) (i + 'A'));
 			group.addComponent(key);
 		}
 
@@ -39,8 +39,8 @@ final class Demos
 	static TouchableGroup createVolumeDemo(double cameraWidth, double cameraHeight, Scalar color)
 	{
 		Grid grid = new Grid(100, 1, cameraWidth, cameraHeight, PADDING);
-		VolumeSlider volumeSlider = new VolumeSlider(new Rect(), color);
-		grid.align(volumeSlider, new Point(40, 0), new Point(60, 0));
+		Rect dimensions = grid.calculateDimensions(new Point(40, 0), new Point(60, 0));
+		VolumeSlider volumeSlider = new VolumeSlider(dimensions, color);
 
 		return new TouchableGroup(volumeSlider);
 	}
--- src/main/java/io/github/jakejmattson/touchcontrol/utils/Grid.java
+++ src/main/java/io/github/jakejmattson/touchcontrol/utils/Grid.java
@@ -1,34 +1,22 @@
 package io.github.jakejmattson.touchcontrol.utils;
 
-import io.github.jakejmattson.touchcontrol.touchables.Touchable;
 import org.opencv.core.*;
 
 public class Grid
 {
-	private final int rowCount, colCount;
-	private final double cellWidth, cellHeight;
+	private final double cellWidth;
+	private final double cellHeight;
 	private final int padding;
 
 	public Grid(int rowCount, int colCount, double totalWidth, double totalHeight, int padding)
 	{
-		this.rowCount = rowCount;
-		this.colCount = colCount;
 		cellWidth = totalWidth / rowCount;
 		cellHeight = totalHeight / colCount;
 		this.padding = padding;
 	}
 
-	public boolean align(Touchable component, Point topLeftCell, Point bottomRightCell)
+	public Rect calculateDimensions(Point topLeftCell, Point bottomRightCell)
 	{
-		if (component == null)
-			return false;
-
-		if (!validatePoint(topLeftCell))
-			return false;
-
-		if (!validatePoint(bottomRightCell))
-			return false;
-
 		Point newTopLeft = new Point();
 		newTopLeft.x = topLeftCell.x * cellWidth + padding;
 		newTopLeft.y = topLeftCell.y * cellHeight + padding;
@@ -37,20 +25,6 @@ public class Grid
 		newBottomRight.x = (bottomRightCell.x + 1) * cellWidth - padding;
 		newBottomRight.y = (bottomRightCell.y + 1) * cellHeight - padding;
 
-		Rect newDim = new Rect(newTopLeft, newBottomRight);
-		component.setDimensions(newDim);
-
-		return true;
-	}
-
-	private boolean validatePoint(Point point)
-	{
-		if (point.x < 0 || point.x > rowCount)
-			return false;
-
-		if (point.y < 0 || point.y > colCount)
-			return false;
-
-		return true;
+		return new Rect(newTopLeft, newBottomRight);
 	}
 }
